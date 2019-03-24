aea69f80c0320c08712fdff6e79b8effa193b4a7
filipzakrocki <42967255+filipzakrocki@users.noreply.github.com>
2019-03-24 17:48:14 +0000

    added CSS Loader for webpack
    +css loader for webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,20 @@
 const path = require('path');
 
 module.exports = {
-  entry: `./src/js/index.js`,
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/main.js'
-  }
+        entry: [`./src/js/index.js`],
+        output: {
+            path: path.resolve(__dirname, 'dist'),
+            filename: 'js/main.js'
+        },
+        devServer: {
+            contentBase: './dist'
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    use: ['style-loader', 'css-loader']
+                }
+            ]
+        }
 };
