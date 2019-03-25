d416c2c0f7d53f3aff8c41d51a61bd93c2a34705
filipzakrocki <42967255+filipzakrocki@users.noreply.github.com>
2019-03-25 17:03:28 +0000

    CSS/HTML main tables

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,27 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-        entry: [`./src/js/index.js`],
-        output: {
-            path: path.resolve(__dirname, 'dist'),
-            filename: 'js/main.js'
-        },
-        devServer: {
-            contentBase: './dist'
-        },
-        module: {
-            rules: [
-                {
-                    test: /\.css$/,
-                    use: ['style-loader', 'css-loader']
+    entry: [`./src/js/index.js`],
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'js/main.js'
+    },
+    devServer: {
+        contentBase: './dist'
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/index.html'
+        })
+    ],
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
                 }
             ]
-        }
+    }
 };
